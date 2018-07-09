72a3186f8d5bffbafe4fa3f1bcc47cc0aa66d7e1
Floyd Wallace <floyd@ip-172-31-1-95.ec2.internal>
2018-07-09 11:38:13 -0700

    commit july changes master

---
+++ scripts/2018_06/early_product_tags.rb
@@ -0,0 +1,58 @@
+#!/bin/ruby
+
+require_relative '../../config/environment'
+
+month_start = Time.local(2018, 6)
+month_end = month_start.end_of_month
+base_tag = { active_start: month_start, active_end: month_end }
+early_tag = { active_start: nil, active_end: month_end }
+
+# sunset the old tags without an active_end
+ProductTag.where(active_end: nil).update_all(active_end: month_start - 1.second)
+
+# main: Rose All Day
+main_3 = 224674119698
+main_5 = 224674086930
+#Can't find them on EllieStaging
+main_3_autorenew = 224675037202
+main_5_autorenew = 224674971666
+
+
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_3, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_5, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_3, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_5, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_3, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_5, tag: 'switchable').update(early_tag)
+
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_3_autorenew, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_5_autorenew, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_3_autorenew, tag: 'skippable')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_5_autorenew, tag: 'skippable')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_3_autorenew, tag: 'switchable')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_5_autorenew, tag: 'switchable')
+
+
+
+# alt 1 Weekend Warrior
+alt1_3 = 224674447378
+alt1_5 = 224674381842
+alt1_3_autorenew = 224675364882
+alt1_5_autorenew = 224675299346
+
+ProductTag.create_with(early_tag).find_or_create_by(product_id: alt1_3, tag: 'current')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: alt1_5, tag: 'current')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: alt1_3_autorenew, tag: 'current')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: alt1_5_autorenew, tag: 'current')
+
+
+# alt 2 Back to Black
+alt2_3 = 224674316306
+alt2_5 = 224674283538
+alt_2_3_autorenew = 224675233810
+alt_2_5_autorenew = 224675135506
+
+ProductTag.create_with(early_tag).find_or_create_by(product_id: alt2_3, tag: 'current')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: alt2_5, tag: 'current')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: alt_2_3_autorenew, tag: 'current')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: alt_2_5_autorenew, tag: 'current')
---
+++ scripts/2018_07/early_product_tags.rb
@@ -0,0 +1,58 @@
+#!/bin/ruby
+
+require_relative '../../config/environment'
+
+month_start = Time.local(2018, 7)
+month_end = month_start.end_of_month
+base_tag = { active_start: month_start, active_end: month_end }
+early_tag = { active_start: nil, active_end: month_end }
+
+# sunset the old tags without an active_end
+ProductTag.where(active_end: nil).update_all(active_end: month_start - 1.second)
+
+# main: Print Paradise
+main_3 = 1446651330618
+main_5 = 1446647234618
+#Can't find them on EllieStaging
+main_3_autorenew = 1508549427258
+main_5_autorenew = 1508553392186
+
+
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_3, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_5, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_3, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_5, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_3, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_5, tag: 'switchable').update(early_tag)
+
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_3_autorenew, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_5_autorenew, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_3_autorenew, tag: 'skippable')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_5_autorenew, tag: 'skippable')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_3_autorenew, tag: 'switchable')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: main_5_autorenew, tag: 'switchable')
+
+
+
+# alt 1 Seeing Stars
+alt1_3 = 1446660702266
+alt1_5 = 1446656999482
+alt1_3_autorenew = 1508540219450
+alt1_5_autorenew = 1508544643130
+
+ProductTag.create_with(early_tag).find_or_create_by(product_id: alt1_3, tag: 'current')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: alt1_5, tag: 'current')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: alt1_3_autorenew, tag: 'current')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: alt1_5_autorenew, tag: 'current')
+
+
+# alt 2 Vitamin Sea
+alt2_3 = 1446669287482
+alt2_5 = 1446664306746
+alt_2_3_autorenew = 1508523147322
+alt_2_5_autorenew = 1508537106490
+
+ProductTag.create_with(early_tag).find_or_create_by(product_id: alt2_3, tag: 'current')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: alt2_5, tag: 'current')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: alt_2_3_autorenew, tag: 'current')
+ProductTag.create_with(early_tag).find_or_create_by(product_id: alt_2_5_autorenew, tag: 'current')
