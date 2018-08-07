8edbacd7b60e6a04b246a4514b24431b73f05ac0
FLWallace105 <fwallace@fambrands.com>
2018-08-07 13:59:13 -0700

    fixed log files moved to 25 minutes pull

--- worker/background_subs.rb
+++ worker/background_subs.rb
@@ -34,6 +34,16 @@ module BackgroundSubs
         my_conn.close
       end
 
+    def twenty_five_min
+        min_ago = 25
+        minutes_ago = DateTime.now - (min_ago/1440.0)
+        twenty_five_minutes_ago_str = minutes_ago.strftime("%Y-%m-%dT%H:%M:%S")
+        puts "Twenty five minutes ago = #{twenty_five_minutes_ago_str}"
+        Resque.logger.info "Twenty five minutes ago = #{twenty_five_minutes_ago_str}"
+        return twenty_five_minutes_ago_str
+
+    end
+
 
     def get_new_subs_last_hour(params)
         Resque.logger = Logger.new("#{Dir.getwd}/logs/get_subs_last_time_period.log")
@@ -42,31 +52,46 @@ module BackgroundSubs
         my_conn =  PG.connect(myuri.hostname, myuri.port, nil, nil, myuri.path[1..-1], myuri.user, myuri.password)
         puts my_conn.inspect
         my_header = params["headers"]
+        Resque.logger.info "BackgroundSubs#get_all_new_subs_last_hour: #{params}"
 
         #Upsert statement
         my_insert = "insert into subscriptions (subscription_id, address_id, customer_id, created_at, updated_at, next_charge_scheduled_at, cancelled_at, product_title, price, quantity, status, shopify_product_id, shopify_variant_id, sku, order_interval_unit, order_interval_frequency, charge_interval_frequency, order_day_of_month, order_day_of_week, raw_line_item_properties, expire_after_specific_number_charges) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21) on conflict (subscription_id) do update set address_id = EXCLUDED.address_id, customer_id = EXCLUDED.customer_id, created_at = EXCLUDED.created_at, updated_at = EXCLUDED.updated_at, next_charge_scheduled_at = EXCLUDED.next_charge_scheduled_at, cancelled_at = EXCLUDED.cancelled_at, product_title = EXCLUDED.product_title, price = EXCLUDED.price, quantity = EXCLUDED.quantity, status = EXCLUDED.status, shopify_product_id = EXCLUDED.shopify_product_id, shopify_variant_id = EXCLUDED.shopify_variant_id, sku = EXCLUDED.sku, order_interval_unit = EXCLUDED.order_interval_unit, order_interval_frequency = EXCLUDED.order_interval_frequency, charge_interval_frequency = EXCLUDED.charge_interval_frequency, order_day_of_month = EXCLUDED.order_day_of_month, order_day_of_week = EXCLUDED.order_day_of_week, raw_line_item_properties = EXCLUDED.raw_line_item_properties, expire_after_specific_number_charges = EXCLUDED.expire_after_specific_number_charges"
         my_conn.prepare('statement1', "#{my_insert}")
 
-        one_hour_ago = DateTime.now - (1.0/24)
-        one_hour_ago_str = one_hour_ago.strftime("%Y-%m-%dT%H:00:00")
-        puts "one hour ago = #{one_hour_ago_str}"
+        #one_hour_ago = DateTime.now - (1.0/24)
+        #one_hour_ago_str = one_hour_ago.strftime("%Y-%m-%dT%H:00:00")
+        #puts "one hour ago = #{one_hour_ago_str}"
+        #Resque.logger.info "one hour ago = #{one_hour_ago_str}"
 
-        subscriptions = HTTParty.get("https://api.rechargeapps.com/subscriptions/count?created_at_min=\'#{one_hour_ago_str}\'", :headers => my_header)
+        
+
+        twenty_five_minutes_ago_str = twenty_five_min
+
+
+        subscriptions = HTTParty.get("https://api.rechargeapps.com/subscriptions/count?created_at_min=\'#{twenty_five_minutes_ago_str}\'", :headers => my_header)
             
         my_count = subscriptions['count'].to_i
-        puts "We have #{my_count} subscriptions created_at this past hour"
+        current_time = DateTime.now.strftime("%Y-%m-%dT%H:%M:%S")
+        puts "Time now = #{current_time}"
+        puts "We have #{my_count} subscriptions created_at this past 25 minutes"
+        Resque.logger.info "Time now = #{current_time}"
+        Resque.logger.info "We have #{my_count} subscriptions created_at this past 25minutes"
 
         start = Time.now    
         page_size = 250
         num_pages = (my_count/page_size.to_f).ceil
         1.upto(num_pages) do |page|
-            mysubs = HTTParty.get("https://api.rechargeapps.com/subscriptions?updated_at_min=\'#{one_hour_ago_str}\'&limit=250&page=#{page}", :headers => my_header)
+            mysubs = HTTParty.get("https://api.rechargeapps.com/subscriptions?updated_at_min=\'#{twenty_five_minutes_ago_str}\'&limit=250&page=#{page}", :headers => my_header)
             recharge_limit = mysubs.response["x-recharge-limit"]
             local_sub = mysubs['subscriptions']
             local_sub.each do |sub|
                 puts "-------------------"
                 puts sub.inspect
                 puts "------------------"
+                Resque.logger.info "-------------------"
+                Resque.logger.info sub.inspect
+                Resque.logger.info "------------------"
+
                 subscription_id = sub['id']
 
                 address_id = sub['address_id']
@@ -97,14 +122,19 @@ module BackgroundSubs
 
                 insert_result = my_conn.exec_prepared('statement1', [ subscription_id, address_id, customer_id, created_at, updated_at, next_charge_scheduled_at, cancelled_at, product_title, price, quantity, status, shopify_product_id, shopify_variant_id, sku, order_interval_unit, order_interval_frequency, charge_interval_frequency, order_day_of_month, order_day_of_week, properties, expire_after])
                 puts insert_result.inspect
+                Resque.logger.info insert_result.inspect
             end
             puts "Done with page #{page}"
+            Resque.logger.info "Done with page #{page}"
             current = Time.now
             duration = (current - start).ceil
             puts "Been running #{duration} seconds"
+            Resque.logger.info "Been running #{duration} seconds"
             determine_limits(recharge_limit, 0.65)
         end
-        puts "All done with last hour's created subscriptions"
+        puts "All done with last 25 minutes created subscriptions"
+        Resque.logger.info "All done with last 25 minutes created subscriptions"
+        
         #exit
 
     end
@@ -112,6 +142,8 @@ module BackgroundSubs
 
     def get_subs_last_hour(params)
         puts "params = #{params.inspect}"
+        #Resque.logger = Logger.new("#{Dir.getwd}/logs/get_updated_subs_last_time_period.log")
+        
         uri = params["uri"]
         myuri = URI.parse(uri)
         my_conn =  PG.connect(myuri.hostname, myuri.port, nil, nil, myuri.path[1..-1], myuri.user, myuri.password)
@@ -119,6 +151,10 @@ module BackgroundSubs
         my_header = params["headers"]
 
         get_new_subs_last_hour(params)
+
+        Resque.logger.info "**************************************"
+        Resque.logger.info "Now doing updated subs pull!"
+        Resque.logger.info "BackgroundSubs#get_subs_last_hour: #{params}"
 
         #insert new record if it does not exist and was not grabbed by earlier job
         my_insert = "insert into subscriptions (subscription_id, address_id, customer_id, created_at, updated_at, next_charge_scheduled_at, cancelled_at, product_title, price, quantity, status, shopify_product_id, shopify_variant_id, sku, order_interval_unit, order_interval_frequency, charge_interval_frequency, order_day_of_month, order_day_of_week, raw_line_item_properties, expire_after_specific_number_charges) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)"
@@ -129,30 +165,43 @@ module BackgroundSubs
         my_temp_update = "update subscriptions set address_id = $1, customer_id = $2, created_at = $3, updated_at = $4, next_charge_scheduled_at = $5, cancelled_at = $6, product_title = $7, price = $8, quantity = $9, status = $10, shopify_product_id = $11, shopify_variant_id = $12, sku = $13, order_interval_unit = $14, order_interval_frequency = $15, charge_interval_frequency = $16, order_day_of_month = $17, order_day_of_week = $18, raw_line_item_properties = $19, expire_after_specific_number_charges = $20 where subscription_id = $21"
         my_conn.prepare('statement2', "#{my_temp_update}")
 
-
-
-        one_hour_ago = DateTime.now - (1.0/24)
-        one_hour_ago_str = one_hour_ago.strftime("%Y-%m-%dT%H:00:00")
-        puts "one hour ago = #{one_hour_ago_str}"
-
+        
+        #one_hour_ago = DateTime.now - (1.0/24)
+        #one_hour_ago_str = one_hour_ago.strftime("%Y-%m-%dT%H:00:00")
+        
+        #puts "one hour ago = #{one_hour_ago_str}"
+        #Resque.logger.info "one hour ago = #{one_hour_ago_str}"
         #my_today = Date.today
-        my_today = "2018-08-03T14:00:00"
+        #my_today = "2018-08-03T14:00:00"
+        
 
-        subscriptions = HTTParty.get("https://api.rechargeapps.com/subscriptions/count?updated_at_min=\'#{one_hour_ago_str}\'", :headers => my_header)
+        twenty_five_minutes_ago_str = twenty_five_min
+
+
+        subscriptions = HTTParty.get("https://api.rechargeapps.com/subscriptions/count?updated_at_min=\'#{twenty_five_minutes_ago_str}\'", :headers => my_header)
             
         my_count = subscriptions['count'].to_i
-        puts "We have #{my_count} subscriptions updated this past hour"
-        
+        current_time = DateTime.now.strftime("%Y-%m-%dT%H:%M:%S")
+        puts "Time now = #{current_time}"
+        puts "We have #{my_count} subscriptions updated this past 25 minutes"
+        Resque.logger.info "Time now = #{current_time}"
+        Resque.logger.info "We have #{my_count} subscriptions updated this past 25 minutes"
+
         start = Time.now
         page_size = 250
         num_pages = (my_count/page_size.to_f).ceil
         1.upto(num_pages) do |page|
-            mysubs = HTTParty.get("https://api.rechargeapps.com/subscriptions?updated_at_min=\'#{one_hour_ago_str}\'&limit=250&page=#{page}", :headers => my_header)
+            mysubs = HTTParty.get("https://api.rechargeapps.com/subscriptions?updated_at_min=\'#{twenty_five_minutes_ago_str}\'&limit=250&page=#{page}", :headers => my_header)
             recharge_limit = mysubs.response["x-recharge-limit"]
             local_sub = mysubs['subscriptions']
             local_sub.each do |sub|
                 puts "-------------------"
                 puts sub.inspect
+                puts "-------------------"
+                Resque.logger.info "-------------------"
+                Resque.logger.info sub.inspect
+                Resque.logger.info "-------------------"
+
                 #update only, API only returns the records UPDATED!
                 #Why RAW SQL? SPEED! Yes Active Record is better. Its slower too
                 subscription_id = sub['id']
@@ -188,9 +237,11 @@ module BackgroundSubs
                         puts myrow.inspect
                         #order_id = myrow['order_id']
                         puts "updating subscription ID #{subscription_id}"
+                        Resque.logger.info "updating subscription ID #{subscription_id}"
     
                         indy_result = my_conn.exec_prepared('statement2', [ address_id, customer_id, created_at, updated_at, next_charge_scheduled_at, cancelled_at, product_title, price, quantity, status, shopify_product_id, shopify_variant_id, sku, order_interval_unit, order_interval_frequency, charge_interval_frequency, order_day_of_month, order_day_of_week, properties, expire_after, subscription_id])
                         puts indy_result.inspect
+                        Resque.logger.info indy_result.inspect
 
                         special_handling_sub_line_items(uri, subscription_id)
                         insert_sub_line_items(uri, raw_properties, subscription_id)
@@ -199,8 +250,13 @@ module BackgroundSubs
                 else
                     puts "Sorry can't update because it does not exist in the database"
                     puts "Inserting record instead into database"
+                    Resque.logger.info "Sorry can't update because it does not exist in the database"
+                    Resque.logger.info "Inserting record instead into database"
+
+
                     insert_result = my_conn.exec_prepared('statement1', [ subscription_id, address_id, customer_id, created_at, updated_at, next_charge_scheduled_at, cancelled_at, product_title, price, quantity, status, shopify_product_id, shopify_variant_id, sku, order_interval_unit, order_interval_frequency, charge_interval_frequency, order_day_of_month, order_day_of_week, properties, expire_after])
                     puts insert_result.inspect
+                    Resque.logger.info insert_result.inspect
                     special_handling_sub_line_items(uri, subscription_id)
                     insert_sub_line_items(uri, raw_properties, subscription_id)
 
@@ -209,17 +265,20 @@ module BackgroundSubs
 
 
 
-                puts "-------------------"
+                
 
                 end
                 puts "Done with page #{page}"
+                Resque.logger.info "Done with page #{page}"
                 current = Time.now
                 duration = (current - start).ceil
                 puts "Been running #{duration} seconds"
+                Resque.logger.info "Been running #{duration} seconds"
                 determine_limits(recharge_limit, 0.65)
             end
-            puts "All done with last hour's updated subscriptions"
-            conn.close
+            puts "All done with last 25 minutes updated subscriptions"
+            Resque.logger.info "All done with last 25 minutes updated subscriptions"
+            my_conn.close
         
 
     end
